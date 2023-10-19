bfcb5717fb76c160032272c02ee09da5ff9e18e9
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-10-19 11:13:28 -0300

    setando

--- index1.php
+++ index1.php
@@ -16,9 +16,13 @@ class Car
     }
 }
 
-$bmw = new Car;
-//$bmw->setColor("black");
-echo $bmw->numberOfTires;
+$bmw = new Car("white");
+
+echo $bmw->getColor(); //Isso imprimirá "branco" à propriedade color
+
+$bmw->setColor("black"); //Isso irá sobrescrever a propriedade de cor para preto
+
+//echo $bmw->numberOfTires;
 echo "<br>";
 echo $bmw->getColor();
 ?>
