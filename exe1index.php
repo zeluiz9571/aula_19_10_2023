b33c968af61399ea5615d817bd42a487a8afacf2
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-10-19 16:16:15 -0300

    Uau

--- exe1index.php
+++ exe1index.php
@@ -1,4 +1,6 @@
 <?php
+//----------------------------------------------------------------//
+
     //Incluindo a classe que criada na pasta "Classes"    
     include("Classes/carro.class.php");
 
@@ -12,6 +14,7 @@
     $carro0->fabricante ="Citroen";
     $carro0->status     ="Disponível";
 
+//----------------------------------------------------------------//
     //Instanciando um novo objeto
     $carro1 = new carro();
 
@@ -21,6 +24,8 @@
     $carro1->cor        ="Amarelo";
     $carro1->fabricante ="Ford";
     $carro1->status     ="Indisponível";
+
+//----------------------------------------------------------------//
 
     //Instanciando um novo objeto
     $carro2 = new carro();
@@ -32,6 +37,8 @@
     $carro2->fabricante ="BMW";
     $carro2->status     ="Disponível";
 
+//----------------------------------------------------------------//
+
     //Instanciando um novo objeto
     $carro3 = new carro();
 
@@ -41,6 +48,8 @@
     $carro3->cor        ="Cinza";
     $carro3->fabricante ="2023";
     $carro3->status     ="Indisponível";
+
+//----------------------------------------------------------------//
 
     //Instanciando um novo objeto
     $carro4 = new carro();
@@ -52,6 +61,8 @@
     $carro4->fabricante ="Ferrari";
     $carro4->status     ="Disponível";
 
+//----------------------------------------------------------------//
+
     //Instanciando um novo objeto
     $carro5 = new carro();
 
@@ -62,6 +73,8 @@
     $carro5->fabricante ="Lmborghini";
     $carro5->status     ="Indisponível";
 
+//----------------------------------------------------------------//
+
     //Imprimindo na tela do usuário os valores do objeto (carro0) acima:
     echo "Carro ". $carro0->status . ": <br/><br/>";
 
@@ -70,7 +83,9 @@
                         de Cor: ". $carro0->cor . "<br>
                             Ano: ". $carro0->ano . "<br /> <hr>";
 
-    //Imprimindo na tela do usuário os valores do objeto (carro0) acima:
+//----------------------------------------------------------------//
+
+    //Imprimindo na tela do usuário os valores do objeto (carro1) acima:
     echo "Carro ". $carro1->status . ": <br/><br/>";
 
     echo "Fabricante do carro: ". $carro1->fabricante . "<br>
@@ -78,7 +93,9 @@
                         de Cor: ". $carro1->cor . "<br>
                             Ano: ". $carro1->ano . "<br> <hr>";
 
-    //Imprimindo na tela do usuário os valores do objeto (carro0) acima:
+//----------------------------------------------------------------//
+
+    //Imprimindo na tela do usuário os valores do objeto (carro2) acima:
     echo "Carro ". $carro2->status . ": <br/><br/>";
 
     echo "Fabricante do carro: ". $carro2->fabricante . "<br>
@@ -86,7 +103,9 @@
                         de Cor: ". $carro2->cor . "<br>
                             Ano: ". $carro2->ano . "<br> <hr>";
 
-    //Imprimindo na tela do usuário os valores do objeto (carro0) acima:
+//----------------------------------------------------------------//
+
+    //Imprimindo na tela do usuário os valores do objeto (carro3) acima:
     echo "Carro ". $carro3->status . ": <br/><br/>";
 
     echo "Fabricante do carro: ". $carro3->fabricante . "<br>
@@ -94,7 +113,9 @@
                         de Cor: ". $carro3->cor . "<br>
                             Ano: ". $carro3->ano . "<br> <hr>";
 
-    //Imprimindo na tela do usuário os valores do objeto (carro0) acima:
+//----------------------------------------------------------------//
+
+    //Imprimindo na tela do usuário os valores do objeto (carro4) acima:
     echo "Carro ". $carro4->status . ": <br/><br/>";
 
     echo "Fabricante do carro: ". $carro4->fabricante . "<br>
@@ -102,12 +123,16 @@
                         de Cor: ". $carro4->cor . "<br>
                             Ano: ". $carro4->ano . "<br> <hr>";
 
-    //Imprimindo na tela do usuário os valores do objeto (carro0) acima:
+//----------------------------------------------------------------//
+
+    //Imprimindo na tela do usuário os valores do objeto (carro5) acima:
     echo "Carro ". $carro5->status . ": <br/><br/>";
 
     echo "Fabricante do carro: ". $carro5->fabricante . "<br>
                         Modelo: ". $carro5->modelo . "<br>
                         de Cor: ". $carro5->cor . "<br>
                             Ano: ". $carro5->ano . "<br> <hr>";
+                            
+//----------------------------------------------------------------//
 
 ?>
