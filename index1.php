e456979c02ed6f04908ffbe09bdfdaf48d35ca25
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-10-19 10:53:01 -0300

    Saí

--- index1.php
+++ index1.php
@@ -4,9 +4,18 @@ class Car
 {
     public $color = 'red';
     public $numberOfTires = 4;
+
+    public function setColor($val){
+        $this->color = $val;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
 }
 
 $bmw = new Car;
 
-echo $bmw ->color;
+echo $bmw->color;
 ?>
