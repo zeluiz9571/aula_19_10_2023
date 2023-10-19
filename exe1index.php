0a39f73fe878bcddd118512e099a0a74e59bc48f
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-10-19 15:49:23 -0300

    Update exe1index.php

--- exe1index.php
+++ exe1index.php
@@ -4,3 +4,28 @@
 
     //Instanciando um novo objeto
     $carro0 = new carro();
+
+    //Atribuindo valores aos atributos
+    $carro0->ano        ="";
+    $carro0->modelo     ="";
+    $carro0->cor        ="";
+    $carro0->fabricante ="";
+    $carro0->status     ="";
+
+    //Instanciando um novo objeto
+    $carro1 = new carro();
+
+    //Atribuindo valores aos atributos
+    $carro1->ano        ="";
+    $carro1->modelo     ="";
+    $carro1->cor        ="";
+    $carro1->fabricante ="";
+    $carro1->status     ="";
+
+    //Imprimindo na tela do usuário os valores do objeto (carro0) acima:
+    echo "Carro ". $carro0->status . ": <br/><br/>";
+
+    echo "Fabricante do carro: ". $carro0->fabricante . "<br?>
+                        Modelo: ". $carro0->modelo "<br />
+                        de Cor: ". $carro0->cor . "<br />
+                            Ano: ". $carro0->ano . "<br /> <hr>";
